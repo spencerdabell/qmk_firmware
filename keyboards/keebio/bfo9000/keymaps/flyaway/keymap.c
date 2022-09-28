25dac4b994a3bf1818c5413bee42c24c02853020
Spencer Dabell <spencerdabell@gmail.com>
2022-09-28 11:19:45 -0700

    First usable layout after adding diodes and fixing last layer

--- keyboards/keebio/bfo9000/keymaps/flyaway/keymap.c
+++ keyboards/keebio/bfo9000/keymaps/flyaway/keymap.c
@@ -5,12 +5,12 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_BASE] = LAYOUT(
-KC_HOME, KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______,       KC_F12 , KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______,
-KC_END , KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______,       KC_EQL , KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, _______, _______,
-KC_PGUP, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, _______,       KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, _______, _______,
-KC_UP  , KC_ENT , KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______, _______,       KC_UP  , KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, _______, _______,
-KC_DOWN, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______,       KC_DOWN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, _______, _______,
-KC_PGDN, KC_LCTL, _______, _______, KC_LGUI, KC_LALT, KC_LEFT, KC_RGHT, _______,       KC_ENT , KC_SPC,  KC_BSPC, _______, KC_RALT, KC_RGUI, KC_RCTL, _______, _______
+KC_ESC , _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_MPLY, _______,       KC_F5  , KC_F6,   KC_F7,   KC_F8,   _______, KC_F9,   KC_F10,  KC_F11,  KC_F12 ,
+_______, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MPRV, _______,       KC_VOLU, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL , KC_BSPC,
+_______, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MNXT, _______,       KC_VOLD, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+_______, _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______, _______,       _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT , _______,
+_______, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______,       _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_END , KC_PGUP,
+_______, KC_LCTL, KC_LCTL, KC_LGUI, KC_LALT, KC_LALT, KC_BSPC, KC_DEL , _______,       KC_ENT , KC_SPC,  KC_BSPC, KC_HOME, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_PGDN
 )
 };
 
