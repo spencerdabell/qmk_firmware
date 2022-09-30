ae482bc51ca188ae971404ebbde2301dc0f8f37c
Spencer Dabell <spencerdabell@gmail.com>
2022-09-30 09:13:06 -0700

    Fix arrows along bottom

--- keyboards/keebio/bfo9000/keymaps/flyaway/keymap.c
+++ keyboards/keebio/bfo9000/keymaps/flyaway/keymap.c
@@ -8,9 +8,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KC_ESC , _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_MPLY, _______,       KC_F5  , KC_F6,   KC_F7,   KC_F8,   _______, KC_F9,   KC_F10,  KC_F11,  KC_F12 ,
 _______, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MPRV, _______,       KC_VOLU, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL , KC_BSPC,
 _______, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MNXT, _______,       KC_VOLD, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-_______, KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______, _______,       _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT , KC_PGUP,
-_______, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______,       _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP  , KC_PGDN,
-KC_LCTL, KC_LCTL, _______, KC_LGUI, KC_LALT, KC_LALT, KC_BSPC, KC_DEL , _______,       KC_ENT , KC_SPC,  KC_BSPC, _______, KC_HOME, KC_END , KC_LEFT, KC_DOWN, KC_RGHT
+KC_PGUP, KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______, _______,       _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT , _______,
+KC_PGDN, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______,       _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, _______, _______,
+KC_LCTL, KC_LCTL, KC_LGUI, _______, KC_LALT, _______, KC_BSPC, KC_DEL , _______,       KC_ENT , KC_SPC,  KC_BSPC, KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_END
 )
 };
 
