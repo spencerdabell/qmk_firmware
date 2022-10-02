d0ab5c5fcb2f3a48be415cca21c810e96e80c753
Spencer Dabell <spencerdabell@gmail.com>
2022-10-02 15:43:30 -0700

    Change the bottom row around again

--- keyboards/keebio/bfo9000/keymaps/flyaway/keymap.c
+++ keyboards/keebio/bfo9000/keymaps/flyaway/keymap.c
@@ -10,7 +10,7 @@ _______, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MPRV, _______,
 _______, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MNXT, _______,       KC_PGUP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
 _______, KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_VOLU, _______,       KC_PGDN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT , _______,
 _______, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_VOLD, _______,       _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, _______, _______,
-KC_LCTL, KC_LCTL, KC_LGUI, _______, KC_LALT, KC_BSPC, KC_ENT , KC_DEL , _______,       KC_ENT , KC_SPC,  KC_BSPC, KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_END
+_______, KC_LCTL, KC_LGUI, _______, KC_LALT, KC_LALT, KC_SPC , KC_DEL , _______,       KC_BSPC, KC_ENT,  KC_RCTL, KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_END
 )
 };
 
