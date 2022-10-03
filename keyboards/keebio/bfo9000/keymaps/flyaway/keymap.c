5c2ff5fbf32f2520426ead91a0fd321d3f1b8695
Spencer Dabell <spencerdabell@gmail.com>
2022-10-03 14:34:22 -0700

    Add home row modifiers

--- keyboards/keebio/bfo9000/keymaps/flyaway/keymap.c
+++ keyboards/keebio/bfo9000/keymaps/flyaway/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KC_ESC , _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_MPLY, _______,       KC_F5  , KC_F6,   KC_F7,   KC_F8,   _______, KC_F9,   KC_F10,  KC_F11,  KC_F12 ,
 _______, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MPRV, _______,       _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL , KC_BSPC,
 _______, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MNXT, _______,       _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-_______, KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_VOLU, _______,       _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT , KC_PGUP,
+_______, KC_LCTL,CTL_T(KC_A),GUI_T(KC_S),ALT_T(KC_D),SFT_T(KC_F),KC_G,KC_VOLU,_______,_______,KC_H,SFT_T(KC_J),ALT_T(KC_K),GUI_T(KC_L),CTL_T(KC_SCLN),KC_QUOT, KC_ENT , KC_PGUP,
 _______, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_VOLD, _______,       _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP  , KC_PGDN,
 _______, _______, _______, _______, KC_LGUI, KC_LALT, KC_ENT , KC_DEL , _______,       KC_BSPC, KC_SPC,  KC_RCTL, _______, KC_HOME, KC_END , KC_LEFT, KC_DOWN, KC_RGHT
 )
