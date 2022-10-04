5de7b78c057034b4ee481d75bc4dcea43b8e6609
Spencer Dabell <spencerdabell@gmail.com>
2022-10-03 20:41:53 -0700

    Add ignore mod tap interrupt for faster regular typing, otherwise it was chaos!

--- keyboards/keebio/bfo9000/keymaps/flyaway/config.h
+++ keyboards/keebio/bfo9000/keymaps/flyaway/config.h
@@ -25,3 +25,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Select hand configuration */
 // #define MASTER_RIGHT
 // #define EE_HANDS
+
+#define TAPPING_TERM 200
+// #define TAPPING_TERM 300
+
+#define IGNORE_MOD_TAP_INTERRUPT
