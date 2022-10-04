8e36d0064b8c9574ce6b8f2a83a33cf2d2ba4114
Spencer Dabell <spencerdabell@gmail.com>
2022-10-04 14:59:58 -0700

    Add mouse layer Remove several unused keys Move shifts to thumbs Move fn keys to func layer, which to be honest i'm not even sure how to activate right now Swap return and space. its going to cause chaos i think but i want to try it for now

--- keyboards/keebio/bfo9000/keymaps/flyaway/keymap.c
+++ keyboards/keebio/bfo9000/keymaps/flyaway/keymap.c
@@ -1,8 +1,10 @@
 #include QMK_KEYBOARD_H
 
-#define _BL 0
-#define _FN  1
-#define _L2  2
+#define _BASE 0
+#define _NAVI 1
+#define _MOUS 2
+#define _FUNC 3
+// #define _NUM  2
 
 #define CT CTL_T
 #define GT GUI_T
@@ -11,29 +13,37 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[_BL] = LAYOUT(
-KC_ESC , _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______,       KC_F5  , KC_F6,   KC_F7,   KC_F8,   _______, KC_F9,   KC_F10,  KC_F11,  KC_F12 ,
-_______, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______,       _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL , KC_BSPC,
-_______, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, _______,       _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-_______, KC_ESC,  CT(KC_A),GT(KC_S),AT(KC_D),ST(KC_F),KC_G,    _______, _______,       _______, KC_H, ST(KC_J),AT(KC_K),GT(KC_L),CT(KC_SCLN),KC_QUOT, KC_ENT , KC_PGUP,
-_______, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______,       _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP  , KC_PGDN,
-_______, LT(_FN,KC_MPLY),KC_MPRV,KC_MNXT,_______,LT(_FN,KC_TAB),LT(_FN,KC_ENT),KC_DEL,_______,       KC_BSPC, LT(_FN,KC_SPC),  KC_TAB,  _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT
+[_BASE] = LAYOUT(
+        KC_ESC , _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______,        KC_F5  , KC_F6,   KC_F7,   KC_F8,   _______, KC_F9,   KC_F10,  KC_F11,  KC_F12 ,
+        _______, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______,        _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL , KC_BSPC,
+        _______, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, _______,        _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+_______, TG(_NAVI),CT(KC_A),GT(KC_S),AT(KC_D),LT(_NAVI, KC_F),LT(_MOUS,KC_G),_______,_______,   _______, KC_H,    KC_J,    KC_K,    KC_L, CT(KC_SCLN),KC_QUOT, KC_ENT , _______,
+        _______, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______,        _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP  , _______,
+        _______, LT(_NAVI,KC_MPLY),KC_MPRV,KC_MNXT,_______,KC_TAB,ST(KC_SPC),KC_DEL,_______,    KC_BSPC, ST(KC_ENT),KC_TAB,_______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT
 ),
-[_FN] = LAYOUT(
-_______, _______, _______, _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______,
-_______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______,       _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______, _______,
-_______, _______, KC_F11,  KC_F12,  _______, _______, _______, _______, _______,       _______, _______, KC_PGUP, KC_UP,   KC_PGDN, _______, _______, _______, _______,
-_______, _______, KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, _______, _______, _______,       _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  _______, _______, _______,
-_______, _______, _______, _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______,
-_______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______
-), 
-[_L2] = LAYOUT(
-_______, _______, _______, _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______,
-_______, _______, _______, _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______,
-_______, _______, _______, _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______,
-_______, _______, _______, _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______,
-_______, _______, _______, _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______,
-_______, _______, _______, _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______
+[_NAVI] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______, KC_PGUP, KC_UP,   KC_PGDN, _______, _______, _______, _______,
+        _______, _______, KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, _______, _______, _______,        _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
+[_MOUS] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______, KC_WH_U, KC_MS_U, KC_WH_D, _______, _______, _______, _______,
+        _______, _______, KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, _______, _______, _______,        _______, KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN2,  _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, KC_BTN3, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
+[_FUNC] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______,        _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______, _______,
+        _______, _______, KC_F11,  KC_F12,  _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 };
 
@@ -49,6 +59,22 @@ _______, _______, _______, _______, _______, _______, _______, _______, _______,
 //     KC_LEFT, KC_DOWN, KC_RGHT, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  KC_ENT,        KC_BSPC, KC_SPC,  KC_SPC,  KC_RGUI, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
 // )
 // };
+
+/*
+DF(layer)	Set the base (default) layer
+MO(layer)	Momentarily turn on layer when pressed (requires KC_TRNS on destination layer)
+OSL(layer)	Momentarily activates layer until a key is pressed. See One Shot Keys for details.
+LT(layer, kc)	Turn on layer when held, kc when tapped
+TG(layer)	Toggle layer on or off
+TO(layer)	Turns on layer and turns off all other layers, except the default layer
+TT(layer)	Normally acts like MO unless it’s tapped multiple times, which toggles layer on
+
+*/
+
+
+
+
+
 
 
 /*
@@ -105,13 +131,6 @@ QK_GRAVE_ESCAPE	QK_GESC	Escape when pressed, ` when Shift or GUI are held
 CAPS_WORD	CAPSWRD	Toggles Caps Word  toggles caps lock until end of word like space or period
 KC_LOCK	Hold down the next key pressed, until the key is pressed again -- very powerful and awesome (hold forward till unpressed)
 
-DF(layer)	Set the base (default) layer
-MO(layer)	Momentarily turn on layer when pressed (requires KC_TRNS on destination layer)
-OSL(layer)	Momentarily activates layer until a key is pressed. See One Shot Keys for details.
-LT(layer, kc)	Turn on layer when held, kc when tapped
-TG(layer)	Toggle layer on or off
-TO(layer)	Turns on layer and turns off all other layers, except the default layer
-TT(layer)	Normally acts like MO unless it’s tapped multiple times, which toggles layer on
 
 leader key - input combined keys one at a time
 
