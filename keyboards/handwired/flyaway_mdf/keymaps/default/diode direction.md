33af28f87c3b6700d68a1a4e156baaf48003ae2c
Spencer Dabell <spencerdabell@gmail.com>
2024-01-12 12:24:50 -0800

    use layout hack

---
+++ keyboards/handwired/flyaway_mdf/keymaps/default/diode direction.md
@@ -0,0 +1,24 @@
+diode direction:
+col2row or row2col
+
+col2row is the typical setup for large keyboards, because the scanning needs a lot of ports?
+why isn't it 
+
+lets just do col2row since thats typical
+
+wont the scanning be more precise if its the other way?
+
+(depending on the wiring)
+333333 or 33444
+
+row2col would be
+vs 666
+seems 2x as fast
+
+
+looks like corrugated flyaway is col2row
+
+lets do row2col for this new build, which i think allows just the diodes to orient the other way
+
+
+
--- keyboards/handwired/flyaway_mdf/keymaps/default/keymap.c
+++ keyboards/handwired/flyaway_mdf/keymaps/default/keymap.c
@@ -178,16 +178,11 @@ enum layer {
     L20, L21, L22, L23, L24, R21, R22, R23, R24, R25, \
                    L25, L15, R10, R20   \
     ) \
-{\
-        { L00, L01, L02, L03, L04, U_}, \
-        { L10, L11, L12, L13, L14, L15}, \
-        { L20, L21, L22, L23, L24, L25}, \
-        { U_,  R01, R02, R03, R04, R05},  \
-        { R10, R11, R12, R13, R14, R15},  \
-        { R20, R21, R22, R23, R24, R25}  \
-}
-
-
+LAYOUT_split_5x3_2_info( \
+    L00, L01, L02, L03, L04, U_,  U_,  R01, R02, R03, R04, R05, \
+    L10, L11, L12, L13, L14, L15, R10, R11, R12, R13, R14, R15, \
+    L20, L21, L22, L23, L24, L25, R20, R21, R22, R23, R24, R25 \
+    ) \
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
