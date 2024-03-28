4f395b8afed81758c1bbe67b49e2978e2e8e4e8c
Spencer Dabell <spencerdabell@gmail.com>
2024-03-27 22:04:18 -0700

    combine fn and nav2

--- keyboards/handwired/flyaway_mdf/keymaps/default/keymap.c
+++ keyboards/handwired/flyaway_mdf/keymaps/default/keymap.c
@@ -7,9 +7,8 @@
 enum layer {
     L_ALPHA = 0, //
     L_NUMSYM,
-    L_FN,
     L_NAVI,
-    L_NAVI2,
+    L_FN,
     L_MOUSE
 };
 
@@ -26,7 +25,7 @@ enum layer {
 #define MS_D LT(L_MOUSE, KC_D)
 #define MS_ESC LT(L_MOUSE, KC_ESC)
 #define FN_L LT(L_FN, KC_L)
-#define FN_ESC LT(L_FN, KC_ESC)
+#define FN_SPC LT(L_FN, KC_SPC)
 #define NS_R LT(L_NUMSYM, KC_R)
 
 // nav
@@ -57,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Z,    KC_W,    KC_G,    KC_F,    KC_K,           KC_QUOT, KC_J,    KC_U,    KC_Y,    KC_Q,
     CT_N,    AT_S,    ST_T,    NV_H,    KC_V,           KC_DOT,  KC_A,    ST_E,    AT_I,    CT_O,
     GT_P,    GT_C,    NV_M,    MS_D,    KC_B,           KC_COMM, KC_X,    KC_SLSH, GT_TAB,  GT_ENT,
-                               NV2_SPC, KC_ESC,         FN_L,    NS_R
+                               FN_SPC,  KC_ESC,         KC_L,    NS_R
 ),
 [L_NUMSYM] = LAYOUT_532(
     KC_LBRC, KC_LT,   KC_GT,   KC_RBRC, KC_DLR,         KC_PLUS, KC_AMPR, KC_HASH, KC_DQUO, KC_GRV,
@@ -65,23 +64,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
                                KC_UNDS, KC_BSLS,        U______, ___H___
 ),
-[L_FN] = LAYOUT_532(
-    U______, U______, U______, KC_PSCR, U______,        U______, KC_MPRV, KC_MPLY, KC_MNXT, U______,
-    U______, U______, CW_TOGG, KC_CAPS, U______,        U______, KC_MRWD, KC_VOLD, KC_VOLU, KC_MFFD,
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-                               KC_F11,  KC_F12,         ___H___, U______
-),
 [L_NAVI] = LAYOUT_532(
     U______, KC_MCTL, U______, U______, U______,        U______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
     Ct_BSPC, Ct_Z,    Ct_Y,    ___H___, U______,        U______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
     U______, U______, U______, U______, U______,        U______, Ct_LEFT, KC_BSPC, KC_DEL,  Ct_RGHT,
                                ST_____, U______,        Ct_BSPC, KC_BSPC
 ),
-[L_NAVI2] = LAYOUT_532(
-    U______, U______, U______, U______, U______,        U______, U______, Gu_END,  Gu_HOME, U______,
-    U______, U______, U______, U______, U______,        U______, Ct_PGUP, CA_DOWN, CA_UP,   Ct_PGDN,
-    U______, U______, U______, U______, U______,        U______, U______, U______, U______, U______,
-                               ___H___, U______,        U______, U______
+[L_FN] = LAYOUT_532(
+    U______, KC_MPRV, KC_MPLY, KC_MNXT, U______,        U______, Gu_END,  CW_TOGG, KC_CAPS, KC_PSCR,
+    KC_MRWD, KC_VOLD, KC_VOLU, KC_MFFD, U______,        U______, Ct_PGUP, CA_DOWN, CA_UP,   Ct_PGDN,
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+                               ___H___, U______,        KC_F11,  KC_F12
 ),
 [L_MOUSE] = LAYOUT_532(
     U______, U______, U______, U______, U______,        U______, U______, KC_BTN1, KC_BTN2, U______,
@@ -90,7 +83,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                ST_____, U______,        U______, U______
 ),
 };
-
 
 enum combos {
     CB_BOOT
@@ -107,7 +99,7 @@ uint16_t COMBO_LEN = CB_LENGTH;
 #define COMBO_3(NAME, KEY1, KEY2, KEY3)  const uint16_t PROGMEM NAME[] = {KEY1, KEY2, KEY3, COMBO_END};
 COMBO_3(BOOT,      KC_G, KC_F, KC_K);
 COMBO_2(Name,      AT_S, GT_P);
-COMBO_2(Email,     NV_M, FN_L);
+COMBO_2(Email,     NV_M, KC_L);
 COMBO_2(PassLinux, KC_J, KC_Q);
 COMBO_2(Tilde,     KC_SLSH, GT_TAB);
 COMBO_2(Caps,      NV_H, ST_E);
